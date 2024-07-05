06272d9d64389821cca73b12f88fc83e4894422f
Bolgrad1990 <bolgrad1990@yandex.ru>
2024-07-05 13:25:49 +0300

    release add and remove cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
+const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
+
+
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+
+  const buttonDel = cardElement.querySelector('.card__delete-button');
+  buttonDel.addEventListener('click', deleteCard);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+  const evtTarg = evt.target.closest('.card');
+  evtTarg.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+  initialCards.forEach((item) => {
+    const cardElement = createCard(item, deleteCard);
+    cardContainer.append(cardElement);
+  })
